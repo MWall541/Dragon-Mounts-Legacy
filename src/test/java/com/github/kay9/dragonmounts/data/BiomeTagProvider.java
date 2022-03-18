a1acefde4aca4d8d65bef1508b5858d286be8afc
Kqy9 <32242079+Kqy9@users.noreply.github.com>
2022-03-18 12:55:51 -0400

    update biome tag gen to new provider.

--- src/test/java/com/github/kay9/dragonmounts/data/BiomeTagProvider.java
+++ src/test/java/com/github/kay9/dragonmounts/data/BiomeTagProvider.java
@@ -2,22 +2,21 @@ package com.github.kay9.dragonmounts.data;
 
 import com.github.kay9.dragonmounts.DragonMountsLegacy;
 import net.minecraft.core.Registry;
-import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.tags.TagsProvider;
+import net.minecraft.data.tags.BiomeTagsProvider;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Biomes;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
-public class BiomeTagProvider extends TagsProvider<Biome>
+public class BiomeTagProvider extends BiomeTagsProvider
 {
     public static final TagKey<Biome> FOREST_DRAGON_BIOMES = key("forest_dragon_biomes");
     public static final TagKey<Biome> NETHER_DRAGON_BIOMES = key("nether_dragon_biomes");
 
     public BiomeTagProvider(DataGenerator gen, ExistingFileHelper validator)
     {
-        super(gen, BuiltinRegistries.BIOME, DragonMountsLegacy.MOD_ID, validator, "worldgen/biomes");
+        super(gen, DragonMountsLegacy.MOD_ID, validator);
     }
 
     @Override
@@ -25,12 +24,6 @@ public class BiomeTagProvider extends TagsProvider<Biome>
     {
         tag(FOREST_DRAGON_BIOMES).add(Biomes.JUNGLE, Biomes.SPARSE_JUNGLE, Biomes.BAMBOO_JUNGLE);
         tag(NETHER_DRAGON_BIOMES).add(Biomes.BASALT_DELTAS, Biomes.CRIMSON_FOREST, Biomes.NETHER_WASTES, Biomes.SOUL_SAND_VALLEY, Biomes.WARPED_FOREST);
-    }
-
-    @Override
-    public String getName()
-    {
-        return "DM:L Biome Tags";
     }
 
     private static TagKey<Biome> key(String name)

f8fe9630d9627aea82058b161563557038468905
Anim Mallon <fieryava@gmail.com>
2022-12-25 05:13:50 -0500

    Allow dragons to swim

--- src/main/java/com/github/kay9/dragonmounts/dragon/TameableDragon.java
+++ src/main/java/com/github/kay9/dragonmounts/dragon/TameableDragon.java
@@ -125,6 +125,9 @@ public class TameableDragon extends TamableAnimal implements Saddleable, FlyingA
         flyingNavigation = new FlyingPathNavigation(this, level);
         groundNavigation = new GroundPathNavigation(this, level);
 
+        flyingNavigation.setCanFloat(true);
+        groundNavigation.setCanFloat(true);
+
         navigation = groundNavigation;
     }
 
@@ -149,6 +152,7 @@ public class TameableDragon extends TamableAnimal implements Saddleable, FlyingA
     protected void registerGoals() // TODO: Much Smarter AI and features
     {
 //        goalSelector.addGoal(1, new DragonLandGoal(this));
+        goalSelector.addGoal(1, new FloatGoal(this));
         goalSelector.addGoal(2, new SitWhenOrderedToGoal(this));
         goalSelector.addGoal(3, new MeleeAttackGoal(this, 1, true));
 //        goalSelector.addGoal(4, new DragonBabuFollowParent(this, 10));

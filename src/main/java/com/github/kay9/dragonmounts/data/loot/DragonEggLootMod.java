51f7f3852f9ed65608bf8ae029d5a2fdc4604495
Kay9Unit <32242079+Kay9Unit@users.noreply.github.com>
2024-04-02 11:04:23 -0400

    egg chance tweaks

--- src/main/java/com/github/kay9/dragonmounts/data/loot/DragonEggLootMod.java
+++ src/main/java/com/github/kay9/dragonmounts/data/loot/DragonEggLootMod.java
@@ -17,8 +17,6 @@ import net.minecraftforge.common.loot.IGlobalLootModifier;
 import net.minecraftforge.common.loot.LootModifier;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-
 import static com.github.kay9.dragonmounts.dragon.breed.DragonBreed.BuiltIn.*;
 
 public class DragonEggLootMod extends LootModifier
@@ -29,11 +27,11 @@ public class DragonEggLootMod extends LootModifier
 
     public record Target(ResourceKey<DragonBreed> forBreed, ResourceLocation target, double chance) {}
     public static Target[] BUILT_IN_CHANCES = new Target[]{
-            new Target(AETHER, BuiltInLootTables.SIMPLE_DUNGEON, 0.2),
-            new Target(FIRE, BuiltInLootTables.DESERT_PYRAMID, 0.085),
+            new Target(AETHER, BuiltInLootTables.SIMPLE_DUNGEON, 0.15),
+            new Target(FIRE, BuiltInLootTables.DESERT_PYRAMID, 0.075),
             new Target(FOREST, BuiltInLootTables.JUNGLE_TEMPLE, 0.3),
             new Target(GHOST, BuiltInLootTables.WOODLAND_MANSION, 0.2),
-            new Target(GHOST, BuiltInLootTables.ABANDONED_MINESHAFT, 0.075),
+            new Target(GHOST, BuiltInLootTables.ABANDONED_MINESHAFT, 0.095),
             new Target(ICE, BuiltInLootTables.IGLOO_CHEST, 0.2),
             new Target(NETHER, BuiltInLootTables.BASTION_TREASURE, 0.35),
             new Target(WATER, BuiltInLootTables.BURIED_TREASURE, 0.175)

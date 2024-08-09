9296c91a7a94e2a0f7cec59ac583fd58d7188862
Kay9Unit <32242079+Kay9Unit@users.noreply.github.com>
2024-08-08 23:02:41 -0400

    guard local player nullability; fixes #240

--- src/main/java/com/github/kay9/dragonmounts/client/KeyMappings.java
+++ src/main/java/com/github/kay9/dragonmounts/client/KeyMappings.java
@@ -5,6 +5,7 @@ import com.github.kay9.dragonmounts.DragonMountsLegacy;
 import com.github.kay9.dragonmounts.dragon.TameableDragon;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
 import org.lwjgl.glfw.GLFW;
 
@@ -12,13 +13,15 @@ import java.util.function.Consumer;
 
 public class KeyMappings
 {
-    public static final KeyMapping FLIGHT_DESCENT_KEY = keymap("flight_descent", GLFW.GLFW_KEY_Z, "key.categories.movement");
-    public static final KeyMapping CAMERA_CONTROLS = keymap("camera_flight", GLFW.GLFW_KEY_F6, "key.categories.movement");
+    private static final String KEY_CATEGORY = "key.category." + DragonMountsLegacy.MOD_ID;
+
+    public static final KeyMapping FLIGHT_DESCENT_KEY = keymap("flight_descent", GLFW.GLFW_KEY_Z);
+    public static final KeyMapping CAMERA_CONTROLS = keymap("camera_flight", GLFW.GLFW_KEY_F6);
 
     @SuppressWarnings({"ConstantConditions"})
-    private static KeyMapping keymap(String name, int defaultMapping, String category)
+    private static KeyMapping keymap(String name, int defaultMapping)
     {
-        return new KeyMapping(String.format("key.%s.%s", DragonMountsLegacy.MOD_ID, name), defaultMapping, category);
+        return new KeyMapping(String.format("key.%s.%s", DragonMountsLegacy.MOD_ID, name), defaultMapping, KEY_CATEGORY);
     }
 
     public static void registerKeybinds(Consumer<KeyMapping> registrar)
@@ -29,12 +32,14 @@ public class KeyMappings
 
     public static void handleKeyPress(int key, int action)
     {
+        LocalPlayer player = Minecraft.getInstance().player;
         if (key == CAMERA_CONTROLS.getKey().getValue()
                 && action == GLFW.GLFW_PRESS
-                && Minecraft.getInstance().player.getVehicle() instanceof TameableDragon d)
+                && player != null
+                && player.getVehicle() instanceof TameableDragon d)
         {
             DMLConfig.CAMERA_DRIVEN_FLIGHT.set(!DMLConfig.cameraDrivenFlight());
-            Minecraft.getInstance().player.displayClientMessage(Component.translatable("mount.dragon.camera_controls." + (DMLConfig.cameraDrivenFlight()? "enabled" : "disabled"), d.getDisplayName()), true);
+            player.displayClientMessage(Component.translatable("mount.dragon.camera_controls." + (DMLConfig.cameraDrivenFlight()? "enabled" : "disabled"), d.getDisplayName()), true);
         }
     }
 }
